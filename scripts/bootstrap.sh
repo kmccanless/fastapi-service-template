770927e3cd05f868f9b92e248ecdbb789691de73
Keith McCanless <keith@testdouble.com>
2025-04-23 14:23:05 -0400

    mv to bootstrap

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -2,21 +2,11 @@
 
 set -e
 
-if ! command -v mise &> /dev/null; then
-  echo "Mise not found. Installing..."
-  curl https://mise.run | bash
-  source "$HOME/.bashrc"
-fi
-
 mise install
+poetry env use python{{ python_version }}
+poetry install
+pre-commit install
 
-if ! command -v copier &> /dev/null; then
-  echo "Copier not found after mise install. Please check installation."
-  exit 1
-fi
-
-copier copy . ../new-fastapi-service
-cd ../new-fastapi-service
 
 echo "Initializing git repo..."
 git init
