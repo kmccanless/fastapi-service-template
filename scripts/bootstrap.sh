ace31352e0a7bf25a176ea4ae52ec5ef2a7ee1d0
Keith McCanless <keith@testdouble.com>
2025-04-23 14:32:24 -0400

    rm broken commands

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -3,14 +3,14 @@
 set -e
 
 mise install
-poetry env use python{{ python_version }}
-poetry install
-pre-commit install
+# poetry env use python{{ python_version }}
+# poetry install
+# pre-commit install
 
 
-echo "Initializing git repo..."
-git init
-git add .
-git commit -m "Initial commit from template"
+# echo "Initializing git repo..."
+# git init
+# git add .
+# git commit -m "Initial commit from template"
 
-echo "Remember to create a GitHub repo and push to it."
+# echo "Remember to create a GitHub repo and push to it."
